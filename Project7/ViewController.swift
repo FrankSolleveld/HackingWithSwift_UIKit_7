2c2d376d9817b9be500b30a822c7952cc4694631
FrankSolleveld <fsolleveld@outlook.com>
2021-02-24 13:57:54 +0100

    Initial Commit

---
+++ Project7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Frank Solleveld on 24/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
